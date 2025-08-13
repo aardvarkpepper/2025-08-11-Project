9f5508ab969d22ec8d194e30c5e204f51d207fbd
James Lee <aardvarkpepper@gmail.com>
2025-08-13 06:26:32 -0400

    Cleanup of some comments.  Still to apply dynamic styling to Country page, deploy.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,18 +4,15 @@ import { SearchAndFilterBar } from "../../components/SearchAndFilterBar";
 import { RegionContext } from "../../contexts/contexts";
 import { useFetch } from "../../hooks/useFetch";
 
-
 // import data from '../../data/data.json' with {type: 'json'};
 
 export const HomePage = () => {
   const { region, setRegion } = useContext(RegionContext);
   const navigate = useNavigate();
-  // console.log(`HomePage Region: ${region}`);
   let fetchAddress = 'https://restcountries.com/v3.1/all?fields=name,flags,region,population,capital';
   if (region) {
     fetchAddress = `https://restcountries.com/v3.1/region/${region}?fields=name,flags,region,population,capital`;
   }
-  // console.log('Fetching:', fetchAddress);
 
   const { data, loading, error } = useFetch(fetchAddress);
   console.log('Data received', data);
@@ -28,9 +25,6 @@ export const HomePage = () => {
   } else if (data.meals === null) {
     return <div>Data or category not found.</div>
   }
-
-  //https://restcountries.com/v3.1/region/europe
-  // https://restcountries.com/v3.1/subregion/Northern Europe
 
   // Time running a bit short, or I'd change mapped div to button.
   return (
@@ -54,23 +48,5 @@ export const HomePage = () => {
         </div>
       </div>
     </div>
-    // <div>
-    //   <div>{region}</div>
-    //   <SearchAndFilterBar />
-    //   <div>Home Page</div>
-    //   <div className='cards-container'>
-    //     {data.filter(element => element.region === region).map(element => <div key={element.name} className='dark-alt card'>
-    //       <div className='card-img-container'>
-    //         <img src={element.flags.png} className='of-contain wh100' />
-    //       </div>
-    //       <div className='card-content'>
-    //         <div className='fw-800 card-title'>{element.name}</div>
-    //         <div><span className='fw-800'>Population:</span> {element.population}</div>
-    //         <div><span className='fw-800'>Region:</span> {element.region}</div>
-    //         <div><span className='fw-800'>Capital:</span> {element.capital}</div>
-    //       </div>
-    //     </div>)}
-    //   </div>
-    // </div>
   )
 }
